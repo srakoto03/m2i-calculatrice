7958bd1c96f7dd610f6193a5b0de657509827713
sahobi.rakotomalala <sahobi.rakotomalala@labom2iformation.fr>
2021-04-08 10:07:35 +0200

    add commit to jenkins test

--- lib/src/main/java/com/m2i/devops/Application.java
+++ lib/src/main/java/com/m2i/devops/Application.java
@@ -5,6 +5,9 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello W");
+		
+		// test automatique jenkins
+		System.out.println("Test plannification");
 
 	}
 
