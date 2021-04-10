8a9491c05fe8bb3bc12d762671f7df9c4d679e18
sahobi.rakotomalala <sahobi.rakotomalala@labom2iformation.fr>
2021-04-10 23:26:49 +0200

    fix quality code test

--- lib/src/main/java/com/m2i/devops/Application.java
+++ lib/src/main/java/com/m2i/devops/Application.java
@@ -1,16 +1,16 @@
 package com.m2i.devops;
 
 public class Application {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**
+		 * test d'application
+		 **/
 		System.out.println("Hello W");
 		
-		// test automatique jenkins
+		/**
+		 * test automatique jenkins
+		 */
 		System.out.println("Test plannification");
-		//Calculator calc = new Calculator();
-		//int result = calc.add(3, 6);
-
 	}
 
 }
