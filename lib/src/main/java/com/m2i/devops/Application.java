68b0efd600469e19d11e462956a0c7782d78e56c
sahobi.rakotomalala <sahobi.rakotomalala@labom2iformation.fr>
2021-04-10 23:40:21 +0200

    fix quality code test pmd

--- lib/src/main/java/com/m2i/devops/Application.java
+++ lib/src/main/java/com/m2i/devops/Application.java
@@ -1,4 +1,9 @@
 package com.m2i.devops;
+/**
+ * 
+ * @author Administrateur
+ *
+ */
 
 public class Application {
 	public static void main(String[] args) {
@@ -6,11 +11,9 @@ public class Application {
 		 * test d'application
 		 **/
 		System.out.println("Hello W");
-		
 		/**
 		 * test automatique jenkins
 		 */
 		System.out.println("Test plannification");
 	}
-
 }
